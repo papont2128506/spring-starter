f9caa989494951ec637f15283272edbe1fae8357
Andrey Trusov <trusov@haulmont.com>
2022-05-22 23:14:43 +0400

    Init project

---
+++ src/test/java/ru/papont/springstarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.papont.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
