2eb3a1623c795ec2a936b93e936c23d676193dd2
Andrey Trusov <trusov@haulmont.com>
2022-05-23 00:52:02 +0400

    @Bean sample 1

--- src/main/java/ru/papont/springstarter/SpringConfig.java
+++ src/main/java/ru/papont/springstarter/SpringConfig.java
@@ -1,11 +1,17 @@
 package ru.papont.springstarter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class SpringConfig {
+
+    @Bean("carBean")
+    public Car getCar() {
+        return new Car();
+    }
 
 
 }
